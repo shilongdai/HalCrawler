88435c3405e896db7b8abcb4d09b6aa80250e79b
Shilong Dai <sdai@viperfish.net>
2018-09-29 00:41:47 -0400

    documented api

--- src/main/java/net/viperfish/crawler/html/TagProcessor.java
+++ src/main/java/net/viperfish/crawler/html/TagProcessor.java
@@ -5,10 +5,27 @@ import java.util.Map;
 import net.viperfish.crawler.exceptions.ParsingException;
 import org.jsoup.nodes.Element;
 
+/**
+ * A processor of a html tag. It is automatically applied to the parsed html by {@link
+ * HttpWebCrawler}.
+ */
 public interface TagProcessor {
 
+	/**
+	 * processes a tag element that matches this processor.
+	 * @param tag the element to process
+	 * @param site the current site
+	 * @return the processed element
+	 * @throws ParsingException if failed to process the element.
+	 */
 	Map<TagDataType, List<TagData>> processTag(Element tag, Site site)
 		throws ParsingException;
 
+	/**
+	 * tests whether this processor should be applied to the specified tag.
+	 * @param element the element to test against.
+	 * @return true if this processor should be applied, false otherwise.
+	 */
 	boolean match(Element element);
+
 }
